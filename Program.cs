e76a49e05786bb072ef8b54387a73cc4de4bfee1
Семиглазов Алексей Викторович <a.semiglazov@tes.local>
2023-03-02 18:03:28 +0300

    create

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+TrasferingFiles app = new TrasferingFiles();
+
+Console.Write("Введите путь к исходному каталогу:");
+string pathToSourceDirectoryString = Console.ReadLine();
+DirectoryInfo pathToSourceDirectory  = new DirectoryInfo(pathToSourceDirectoryString);
+
+Console.Write("Введите путь к исходному каталогу:");
+string pathToDestinationalDirectoryString = Console.ReadLine();
+DirectoryInfo pathToDestinationalDirectory = new DirectoryInfo(pathToDestinationalDirectoryString);
+
---
+++ TrasferingFiles.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using System.IO;
+
+internal class TrasferingFiles{
+
+    private DirectoryInfo? _pathToSourceDirectory;
+    private DirectoryInfo? _pathToDestinationDirectory;
+    public DirectoryInfo? PathToSourceDirectory{
+        get{
+           return _pathToSourceDirectory;
+        }
+        set{
+            if(value != null && value.Exists == true){
+                _pathToDestinationDirectory = value;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Проверьте правильность ввода пути к исходному каталогу");
+                Console.ResetColor();
+            }
+        }
+    }
+    public DirectoryInfo? PathToDestinationDirectory
+    {
+        get
+        {
+            return _pathToSourceDirectory;
+        }
+        set
+        {
+            if (value != null && value.Exists == true) {
+                _pathToDestinationDirectory = value;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Проверьте правильность ввода пути к каталогу назначения");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    public void startProgam() {
+
+    }
+    /*public TrasferingFiles(DirectoryInfo? pathToSourceDirectory, DirectoryInfo? pathToDestinationalDirectory)
+        {
+            
+        }
+    }*/
+}
+
